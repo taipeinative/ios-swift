c4ff56d18ff4669d29e5aa20a3939ba3acb06ff1
Taipei Native <85215798+taipeinative@users.noreply.github.com>
2026-03-03 11:39:57 +0800

    chore: initialize project
    * Initialize the project with the default template.

---
+++ demo-01/demo-01/demo_01App.swift
@@ -0,0 +1,17 @@
+//
+//  demo_01App.swift
+//  demo-01
+//
+//  Created by Taipei Native on 2026/3/3.
+//
+
+import SwiftUI
+
+@main
+struct demo_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
