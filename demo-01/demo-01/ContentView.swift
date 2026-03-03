acbad187bf9b483f34e825bde260817f07d2240a
Taipei Native <85215798+taipeinative@users.noreply.github.com>
2026-03-03 11:39:57 +0800

    chore: initialize project
    * Initialize the project with the default template.

---
+++ demo-01/demo-01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demo-01
+//
+//  Created by Daniel Ho on 2026/3/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demo-01/demo-01/demo_01App.swift
@@ -0,0 +1,17 @@
+//
+//  demo_01App.swift
+//  demo-01
+//
+//  Created by Daniel Ho on 2026/3/3.
+//
+
+import SwiftUI
+
+@main
+struct demo_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
